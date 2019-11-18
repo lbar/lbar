ab2875729104b0268db004378a78aca14ee8bd3d
Loïc <lbar@kage.fr>
2019-11-18 20:41:29 +0100

    👷 execute e2e tests during CI

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('loicbarbier2 app is running!');
+    expect(page.getTitleText()).toEqual('Loïc Barbier');
   });
 
   afterEach(async () => {
